0389800141105c98eb85b8f9b472751315a47de7
isaveenstra <isaveenstra@outlook.com>
2023-11-16 10:17:43 +0100

    I made a repository for smallangle and installed the package in a new environment. I added click to the code to put it in a click.group.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,20 +3,39 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
+@cmd_group.command()
+@click.argument("number")
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help="number of x-values between 0 and 2 pi",
+    show_default=True,  # show default in help
+    )
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@cmd_group.command()
+@click.argument("number")
+@click.option(
+    "-n"
+    "--number"
+    default=1,
+    help="Number that you want the tan of",
+    show_default=True,  # show default in help
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
-
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
