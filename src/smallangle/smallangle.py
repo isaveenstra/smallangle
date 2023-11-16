3cb820fd8fcbd579605ed5ef7b9517c22813b70d
isaveenstra <isaveenstra@outlook.com>
2023-11-16 11:22:22 +0100

    Added docstrings to the functions. If you now ask in the terminal for smallangle sin or tan it will give you all the information that is needed about the inputs and the function.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,41 +1,65 @@
+# Name: Isa Veenstra
+# Student number: 14578522
+# This code creates a click group of functions that calculate the sin 
+# and tan of a number of x-values between 0 and 2 pi 
+
 import click
 import numpy as np
 from numpy import pi
 import pandas as pd
 
+# create a click group smallangle 
 @click.group()
-def cmd_group():
+def smallangle():
     pass
 
-@cmd_group.command()
-@click.argument("number")
+# add the option --number variable that can be used in the function sin
+@smallangle.command()
 @click.option(
     "-n",
     "--number",
-    default=1,
-    help="number of x-values between 0 and 2 pi",
+    default=2,
+    help="Number of x-values between 0 and 2 pi.",
     show_default=True,  # show default in help
     )
+# create a function sin that determines the sin of a number of x-values between 0 and 2 pi 
 def sin(number):
+    """Calculate the sin of a number of x-values between 0 and 2 pi in steps
+    
+    Args: 
+	    number (int): number of x-values between 0 and 2 pi
+    
+    Returns: 
+	    sin of the number of x-values between 0 and 2 pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-@cmd_group.command()
-@click.argument("number")
+# add the option --number variable that can be used in the function tan 
+@smallangle.command()
 @click.option(
-    "-n"
-    "--number"
-    default=1,
-    help="Number that you want the tan of",
+    "-n",
+    "--number",
+    default=2,
+    help="Number of x-values between 0 and 2 pi.",
     show_default=True,  # show default in help
 )
+# create a function tan that determines the tan of a number of x-values between 0 and 2 pi
 def tan(number):
+    """Calculate the tan of a number of x-values between 0 and 2 pi in steps
+
+    Args:
+        number (int): number of x-values between 0 and 2 pi
+
+    Returns: 
+	    tan of the number of x-values between 0 and 2 pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
 if __name__ == "__main__":
-    cmd_group()
+    smallangle()
